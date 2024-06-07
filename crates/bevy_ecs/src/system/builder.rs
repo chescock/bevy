75e2b3942d6298035205c69cd27a5fa33aafa261
Chris Russell <8494645+chescock@users.noreply.github.com>
2024-06-07 12:09:26 -0400

    Enable type inference of closure parameter when building dynamic systems.

--- crates/bevy_ecs/src/system/builder.rs
+++ crates/bevy_ecs/src/system/builder.rs
@@ -427,6 +427,21 @@ mod tests {
     }
 
     #[test]
+    fn multi_param_builder_inference() {
+        let mut world = World::new();
+
+        world.spawn(A);
+        world.spawn_empty();
+
+        let system = (LocalBuilder(0u64), ParamBuilder::local::<u64>())
+            .build_state(&mut world)
+            .build_system(|a, b| *a + *b + 1);
+
+        let result = world.run_system_once(system);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
     fn param_set_builder() {
         let mut world = World::new();
 
@@ -497,7 +512,7 @@ mod tests {
             DynParamBuilder::new::<&Entities>(ParamBuilder),
         ],)
             .build_state(&mut world)
-            .build_system(|mut params: Vec<DynSystemParam>| {
+            .build_system(|mut params| {
                 let local = *params[0].downcast_mut::<Local<usize>>().unwrap();
                 let query_count = params[1]
                     .downcast_mut::<Query<()>>()
